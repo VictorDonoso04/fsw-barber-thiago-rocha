a695c0acf6ee168f1ddf63f2d7575f2e87d2c41b
VictorDonoso04 <victordonoso97@gmail.com>
2024-08-11 22:10:18 -0300

    fsw barber

---
+++ script.js
@@ -0,0 +1,32 @@
+const populares = [
+    {
+        img: 'imagens/populares__photos/rating05.png',
+        nome: 'Vintage Barber',
+        endereco: 'Avenida São Sebastião, 357, São Paulo'
+    },
+    {
+        img: 'imagens/populares__photos/rating.png',
+        nome: 'Vintage Barber',
+        endereco: 'Avenida São Sebastião, 357, São Paulo'
+    },
+    {
+        img: 'imagens/populares__photos/rating01.png',
+        nome: 'Vintage Barber',
+        endereco: 'Avenida São Sebastião, 357, São Paulo'
+    },
+    {
+        img: 'imagens/populares__photos/rating02.png',
+        nome: 'Barbearia Moderno',
+        endereco: 'Rua das Flores, 123, Rio de Janeiro'
+    },
+    {
+        img: 'imagens/populares__photos/rating03.png',
+        nome: 'Barbearia Clássica',
+        endereco: 'Praça da Liberdade, 45, Belo Horizonte'
+    },
+    {
+        img: 'imagens/populares__photos/rating04.png',
+        nome: 'Barbearia Luxo',
+        endereco: 'Avenida Paulista, 1500, São Paulo'
+    }
+];
